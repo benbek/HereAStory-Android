3005e53740f031a4e7e3748ee7c64729cc2592f5
AvitalZipori <avital.zipori@mail.huji.ac.il>
2014-08-02 16:54:02 +0300

    Initial work on add story process

--- src/com/hereastory/CreateStoryActivity.java
+++ src/com/hereastory/CreateStoryActivity.java
@@ -1,92 +1,48 @@
 package com.hereastory;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hereastory.shared.Location;
+import com.hereastory.shared.Story;
 
 public class CreateStoryActivity extends Activity {
-	private static final int MEDIA_TYPE_IMAGE = 1;
-	private static final int MEDIA_TYPE_VIDEO = 2;
-	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private Uri fileUri;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_story);
+		// TODO: verify can't press next without description
+
+		setupNextButton();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.create_story, menu);
-		return true;
+    private void setupNextButton() {
+    	final Button button = (Button) findViewById(R.id.buttonNewStoryDescriptionNext);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Story story = new Story();	
+            	story.setDescription(getDescription());
+            	story.setLocation(getLocation());
+				Intent intent = new Intent(getApplicationContext(), CapturePictureActivity.class);
+    			intent.putExtra(IntentConsts.STORY_OBJECT, story);
+    			startActivity(intent);
+            }
+        });
 	}
-	
-	
-	private void capturePicture() {
-	    // create Intent to take a picture and return control to the calling application
-	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-	    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-	    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-	    // start the image capture Intent
-	    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-	}
-
-	/** Create a file Uri for saving an image or video */
-	private Uri getOutputMediaFileUri(int type){
-	      return Uri.fromFile(getOutputMediaFile(type));
-	}
-
-	/** Create a File for saving an image or video */
-	private File getOutputMediaFile(int type){
-	    // To be safe, you should check that the SDCard is mounted
-	    // using Environment.getExternalStorageState() before doing this.
-
-	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-	              Environment.DIRECTORY_PICTURES), "HereAStory");
-	    // This location works best if you want the created images to be shared
-	    // between applications and persist after your app has been uninstalled.
-
-	    // Create the storage directory if it does not exist
-	    if (! mediaStorageDir.exists()){
-	        if (! mediaStorageDir.mkdirs()){
-	            Log.d("HereAStory", "failed to create directory");
-	            return null;
-	        }
-	    }
-
-	    // Create a media file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-	    File mediaFile;
-	    if (type == MEDIA_TYPE_IMAGE){
-	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-	        "IMG_"+ timeStamp + ".jpg");
-	    } else if(type == MEDIA_TYPE_VIDEO) {
-	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-	        "VID_"+ timeStamp + ".mp4");
-	    } else {
-	        return null;
-	    }
-
-	    return mediaFile;
+    
+	private String getDescription() {
+		EditText descriptionEditText = (EditText) findViewById(R.id.editTextNewStoryDescription);
+		String description = descriptionEditText.getText().toString();
+		return description;
 	}
 	
-	private Object captureAudio() {
-		// TODO Auto-generated method stub
-		return null;
+	private Location getLocation() {
+		return new Location(1l, 2l); // TODO
 	}
 
 }
--- src/com/hereastory/MainActivity.java
+++ src/com/hereastory/MainActivity.java
@@ -1,21 +1,60 @@
 package com.hereastory;
 
-import com.hereastory.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupAddStoryButton();
+        setupHearStoryButton();
     }
 
+    private void setupHearStoryButton() {
+    	final Button button = (Button) findViewById(R.id.buttonHearStory);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(getApplicationContext(), HearStoryActivity.class);
+    			intent.putExtra(IntentConsts.STORY_ID, 1); // TODO: real id
+    			startActivityForResult(intent, IntentConsts.HEAR_STORY_CODE);            
+            }
+        });
+		
+	}
 
-    @Override
+	private void setupAddStoryButton() {
+    	final Button button = (Button) findViewById(R.id.buttonCreateStory);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(getApplicationContext(), CreateStoryActivity.class);
+    			startActivityForResult(intent, IntentConsts.CREATE_STORY_CODE);
+            }
+        });
+	}
+	
+	protected void onActivityResult(int reqCode, int resCode, Intent data) {
+		switch (reqCode) {
+		case IntentConsts.CREATE_STORY_CODE:
+			if (resCode == RESULT_OK) {
+				// TODO:
+			}
+			break;
+		case IntentConsts.HEAR_STORY_CODE:
+			if (resCode == RESULT_OK) {
+				// TODO:
+			}
+			break;
+		}
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
