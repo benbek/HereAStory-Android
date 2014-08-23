e154d135fdac69ecefcca30a4b185754cabd670a
Ben <ben.bekerman@gmail.com>
2014-08-23 17:39:20 +0300

    Added the SystemUiHider activity Modified the manifest for Maps v2 API keys and permissions Added the Google Play Services jar to the libs/ directory

--- src/com/hereastory/MapActivity.java
+++ src/com/hereastory/MapActivity.java
@@ -1,12 +1,17 @@
 package com.hereastory;
 
+import com.hereastory.ui.SystemUiHiderActivity;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity {
+/**
+ * The main map activity.
+ */
+public class MapActivity extends SystemUiHiderActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -17,15 +22,18 @@ public class MapActivity extends Activity {
         GoogleMap map = ((MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
 
-        LatLng sydney = new LatLng(-33.867, 151.206);
+        // Set location to Safra Campus, Jerusalem: (31.774476,35.203543)
+        LatLng jerusalem = new LatLng(31.774476, 35.203543);
 
         map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(jerusalem, 13));
 
         map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+                .title("Givat Ram")
+                .snippet("Where geeks prosper.")
+                .position(jerusalem));
 
+        super.setupUiHide(findViewById(R.id.map), findViewById(R.id.fullscreen_content_controls), R.id.dummy_button);
     }
+
 }
