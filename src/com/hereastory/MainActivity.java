e32374fe6712514e55800438a0d302099eb0414a
Tomer Nussbaum <tussbaum@gmail.com>
2014-10-22 14:40:05 +0300

    * drawers to the mapActivity were added Does NOT RUN! * Hearing Story is set with most of the buttons (there is a comment on the parts that are missing * Creating Story has the template waits for buttons

--- src/com/hereastory/MainActivity.java
+++ src/com/hereastory/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.crashlytics.android.Crashlytics;
 import com.hereastory.service.api.OutputFileService;
 import com.hereastory.service.api.OutputFileServiceFactory;
 import com.parse.ParseException;
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		Crashlytics.start(this);
         setContentView(R.layout.activity_main);
 
         OutputFileServiceFactory.init(this);
