607f9ed6af0c07c976f635e39d9a44eeda9bb0b2
Ben Bek <ben.bekerman@gmail.com>
2014-11-01 16:32:01 +0200

    Declusterify - 1st version

--- src/com/hereastory/MapActivity.java
+++ src/com/hereastory/MapActivity.java
@@ -15,12 +15,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.res.Configuration;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.androidmapsextensions.ClusterGroup;
 import com.androidmapsextensions.ClusteringSettings;
 import com.androidmapsextensions.GoogleMap;
 import com.androidmapsextensions.GoogleMap.OnCameraChangeListener;
@@ -38,6 +40,7 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -165,6 +168,7 @@ public class MapActivity extends SystemUiHiderActivity implements GooglePlayServ
 	final PointOfInterestReadHandler markerReader = new POIReader();
 	private PointOfInterestService poiService;
 	
+	private List<Marker> declusterifiedMarkers;
 	protected Map<PointLocation, LimitedPointOfInterest> cachedMarkers = new HashMap<PointLocation, LimitedPointOfInterest>();
 	protected Set<PointLocation> knownPoints = new HashSet<PointLocation>();
 
@@ -268,6 +272,10 @@ public class MapActivity extends SystemUiHiderActivity implements GooglePlayServ
 
 	@Override
 	public boolean onMarkerClick(final Marker clickedMarker) {
+		if (clickedMarker.isCluster()) {
+			declusterify(clickedMarker);
+            return true;
+		}
 		PointLocation loc = clickedMarker.getData();
 		if (loc != null) {
 			clickedMarker.setTitle(this.getString(R.string.marker_loading_title));
@@ -281,6 +289,39 @@ public class MapActivity extends SystemUiHiderActivity implements GooglePlayServ
 		
 		return false;
 	}
+	
+    private void declusterify(Marker cluster) {
+        clusterifyMarkers();
+        declusterifiedMarkers = cluster.getMarkers();
+        LatLng clusterPosition = cluster.getPosition();
+        double distance = calculateDistanceBetweenMarkers();
+        double currentDistance = -declusterifiedMarkers.size() / 2 * distance;
+        for (Marker marker : declusterifiedMarkers) {
+            marker.setClusterGroup(ClusterGroup.NOT_CLUSTERED);
+            LatLng newPosition = new LatLng(clusterPosition.latitude, clusterPosition.longitude + currentDistance);
+            marker.animatePosition(newPosition);
+            currentDistance += distance;
+        }
+    }
+	
+    private double calculateDistanceBetweenMarkers() {
+        Projection projection = map.getProjection();
+        Point point = projection.toScreenLocation(new LatLng(0.0, 0.0));
+        point.x += getResources().getDimensionPixelSize(R.dimen.distance_between_markers);
+        LatLng nextPosition = projection.fromScreenLocation(point);
+        return nextPosition.longitude;
+    }
+	
+    private void clusterifyMarkers() {
+        if (declusterifiedMarkers != null) {
+            for (Marker marker : declusterifiedMarkers) {
+            	PointLocation location = marker.getData();
+                marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                marker.setClusterGroup(ClusterGroup.DEFAULT);
+            }
+            declusterifiedMarkers = null;
+        }
+    }
 	
 	protected void showExceptionDialog(Exception e) {
 	     AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -403,6 +444,7 @@ public class MapActivity extends SystemUiHiderActivity implements GooglePlayServ
 	@Override
 	public void onCameraChange(CameraPosition position) {
 		addMarkersAtLocation(position.target, position.zoom);
+		clusterifyMarkers();
 	}
 
 	@Override
